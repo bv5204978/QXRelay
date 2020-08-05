34b52e480997401062abb52b2a48c9eb13aaacde
霜繁 <>
2020-08-05 09:41:44 +0800

    history price

--- JS/Rewrite/jingdong.price.js
+++ JS/Rewrite/jingdong.price.js
@@ -1,5 +1,5 @@
 /**
- * 京东历史价格 4.10
+ * 京东历史价格 8.1
  * 
  * https://github.com/yichahucha/surge/tree/master/jd_price.js
  * 
@@ -15,6 +15,7 @@ README：https://github.com/yichahucha/surge/tree/master
 
 const path1 = "serverConfig";
 const path2 = "wareBusiness";
+const path3 = "basicConfig";
 const consolelog = false;
 const url = $request.url;
 const body = $response.body;
@@ -23,6 +24,15 @@ const $tool = tool();
 if (url.indexOf(path1) != -1) {
     let obj = JSON.parse(body);
     delete obj.serverConfig.httpdns;
+    delete obj.serverConfig.dnsvip;
+    delete obj.serverConfig.dnsvip_v6;
+    $done({ body: JSON.stringify(obj) });
+}
+
+if (url.indexOf(path3) != -1) {
+    let obj = JSON.parse(body);
+    delete obj.data.JDHttpToolKit.httpdns;
+    delete obj.data.JDHttpToolKit.dnsvipV6;
     $done({ body: JSON.stringify(obj) });
 }
 
