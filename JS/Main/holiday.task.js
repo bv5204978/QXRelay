088619c590622a0d2b035f080be89ec6a5679b82
霜繁 <>
2020-08-31 18:29:06 +0800

    update

--- JS/Main/holiday.task.js
+++ JS/Main/holiday.task.js
@@ -11,8 +11,9 @@
  */
 
 
-let imps = [{"name":"霜繁的生日", "start":"2020-10-21"}]
+let imps = [{"name":"霜繁的生日", "start":"10-21"}]
 let gapDay = 35
+
 
 var url = {
     url:"https://raw.githubusercontent.com/bv5204978/QXRelay/master/Api/holiday.json"
@@ -20,28 +21,47 @@ var url = {
 
 $task.fetch(url).then(response => {
     let data = JSON.parse(response.body)
+// fetch(url.url).then(response => response.json()).then(data => {
 
     let imps_list = imps.concat(data)
 
     //当前时间
     let currDate = new Date()
-    let curr = `${currDate.getFullYear()}-${("0" + (currDate.getMonth() + 1)).slice(-2)}-${("0" + currDate.getDate()).slice(-2)}`
+    let currYear = currDate.getFullYear()
+    let currStr = `${currDate.getFullYear()}-${("0" + (currDate.getMonth() + 1)).slice(-2)}-${("0" + currDate.getDate()).slice(-2)}`
 
-    let currTime = new Date(curr).getTime()
-
-    // //校正日期格式
-    // for (var i in imps_list) {
-    //     var imp = imps_list[i]
-    //     imp.
-
-    // }
+    
 
     //排序
-    imps_list.sort((a, b) => (new Date(a.start) - new Date(b.start)));
+    imps_list.sort((a, b) => {
+
+        var aDate;
+        var bDate;
+
+        if (a.start.split("-").length < 3) {
+            aDate = new Date(`${currYear}-${a.start}`)
+        } else {
+            aDate = new Date(a.start)
+        }
+        if (b.start.split("-").length < 3) {
+            bDate = new Date(`${currYear}-${b.start}`)
+        } else {
+            bDate = new Date(b.start)
+        }
+
+        return aDate - bDate
+    });
+
+
+    let currTime = new Date(currStr).getTime()
     
-    var more = ""
+    var more = []
     for (var i in imps_list) {
         var imp = imps_list[i]
+
+        if (imp.start.split("-").length < 3) {
+            imp.start = currYear + "-" + imp.start
+        }
 
         if (imp.end == undefined || imp.end.length == 0 || new Date(imp.end) < new Date(imp.start)) {
             imp.end = imp.start
@@ -50,26 +70,23 @@ $task.fetch(url).then(response => {
         let sTime = new Date(imp.start).getTime()
         let sDay = (sTime - currTime) / 1000 / 3600 / 24
 
-        if (imp.start == imp.end) {
-            if (sDay > 0 && sDay <= gapDay) {
-                more = more + `\n距离【${imp.name}】还有【${sDay}】天`
-            }
-        } else {
-            let eTime = new Date(imp.end).getTime()
-            let eDay = (eTime - currTime) / 1000 / 3600 / 24
+        let eTime = new Date(imp.end).getTime()
+        let eDay = (eTime - currTime) / 1000 / 3600 / 24
 
-            if (sDay <= gapDay) {
-                if (sDay > 0) {
-                    more = more + `\n距离【${imp.name}】还有【${sDay}】天, 为期${eDay - sDay + 1}天`
-                } else if (eDay > 0) {
-                    more = more + `\n距离【${imp.name}】结束还有【${eDay}】天`
-                }
+        if (sDay <= gapDay && eDay > 0) {
+            if (sDay == eDay) {
+                more.push(`\n距离【${imp.name}】还有【${sDay}】天`)
+            } else if (sDay > 0) {
+                more.push(`\n距离【${imp.name}】还有【${sDay}】天, 将为期${eDay - sDay + 1}天`)
+            } else {
+                more.push(`\n距离【${imp.name}】结束还有【${eDay}】天`)
             }
         }
     }
 
+
     if (more.length > 0) {
-        console.log(`倒数日:\n${more}`)
+        console.log(`倒数日:\n${more.toString()}`)
         $notify("倒数日", "", more)
     }
 
