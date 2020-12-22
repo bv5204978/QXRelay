205988047a8df844ffc635e26d06a5679404719f
霜繁 <>
2020-12-22 17:43:21 +0800

    xxys 测试

--- JS/Rewrite/xxys.vip.js
+++ JS/Rewrite/xxys.vip.js
@@ -1,58 +1,65 @@
 /**
- * 小小影视vip 20.8.14
+ * 小小影视vip 20.12.22
  * 
  * @shuangfan
 //  * https://github.com/NobyDa/Script/blob/master/QuantumultX/File/xxys.js
-//  * new https://github.com/Sunert/Scripts/blob/master/Script/xxys.js
+//  * https://github.com/Sunert/Scripts/blob/master/Script/xxys.js
+//  * https://raw.githubusercontent.com/photonmang/quantumultX/master/xxys.js
  * 
  * 
- * MITM = *.xxjjappss.com,*.xjxjappss.com
- * ^https:\/\/[a-z0-9]+\.(xxjj|xjxj)appss\.com\/(ucp\/index|vod\/reqplay|getGlobalData) url script-response-body https://raw.githubusercontent.com/bv5204978/QXRelay/master/JS/Rewrite/xxys.vip.js
+ * MITM = *.xxjjappss.com,*.xjxjappss.com,*.huaerdadi.com
+ * ^https:\/\/[a-z0-9]+\.(xxjjappss|xjxjappss|huaerdadi)\.com\/(ssp-svr\/ssp\/list3|vod\/reqplay|ucp\/index|getGlobalData) url script-response-body https://raw.githubusercontent.com/bv5204978/QXRelay/master/JS/Rewrite/xxys.vip.js
  * 
  * 商店下载easyknowlege 
  */
 
 
-const path1 = "/ucp/index";
-const path2 = "/vod/reqplay/";
+// ucp/index
+// vod/reqplay
+// getGlobalData
 
-const ad = "/getGlobalData";
+// ssp-svr/ssp/list3
+
+const index = "ucp/index";
+const reqplay = "vod/reqplay";
+
+const global = "getGlobalData";
+
+const ssp = "ssp-svr/ssp/list3";
+
 
 let obj = JSON.parse($response.body);
 
-if ($request.url.indexOf(path1) != -1) {
+if ($request.url.indexOf(index) != -1) {
 	obj.data.user["isvip"] = "1";
 	obj.data.user["avatar_url"] = "https://raw.githubusercontent.com/bv5204978/QXRelay/master/Resource/jpg.jpg";
 	obj.data.user["goldcoin"] = "233";
 
-	obj.data.uinfo["down_daily_remainders"] = "233";
-	obj.data.uinfo["play_daily_remainders"] = "233";
-	obj.data.uinfo["goldcoin"] = "233";
-
 	obj.data.uinfo["curr_group"] = "5"//obj.data.groups[obj.data.groups.length - 1];
+	obj.data.uinfo["play_daily_remainders"] = "233";
+	obj.data.uinfo["down_daily_remainders"] = "233";
+
+	obj.data.uinfo["goldcoin"] = "233";
 }
 
-if ($request.url.indexOf(path2) != -1) {
+if ($request.url.indexOf(reqplay) != -1) {
 	obj.retcode = "0";
 	obj.data.lastplayindex = "1";
-	if (obj.data.hasOwnProperty("httpurl_preview")) {
+	if(obj.data.hasOwnProperty("httpurl_preview")){
 		var playurl = obj.data["httpurl_preview"];
 		obj.data["httpurl"] = playurl;
 	};
 }
 
-if ($request.url.indexOf(ad) != -1) {
+if ($request.url.indexOf(global) != -1) {
 	delete obj.data.adrows
 	delete obj.data.adgroups
 	delete obj.data.iOS_adgroups
 	delete obj.data.Android_adgroups
 }
 
+if ($request.url.indexOf(ssp) != -1) {
+	delete obj.data.pmap
+}
+
 $done({ body: JSON.stringify(obj) });
-
-
-
-/*
-// const path1 = "/init";
-// const path2 = "/comment/listing";
-*/
